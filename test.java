9f626a6b8c627b51bf63bb5fe9bc6a5310470874
NUCLEAR1207 <nuclearleh1207@naver.com>
2022-12-19 18:14:00 +0800

    test for first start java

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args){
+        System.out.println("Hello, This is test SMS for start java throw VSCODE.");
+        System.out.println("안녕하세요, 이 메시지는 VSCODE를 통한 자바의 시작을 위한 테스트 메시지 입니다.");
+    }
+}
